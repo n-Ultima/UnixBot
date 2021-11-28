8e28323b59215b77d4b27aeaa1d9595d09a8304b
n-Ultima <nicbarclay05@gmail.com>
2021-11-28 14:01:30 -0600

    Move ownerservice into Core

--- Unix.ClientApp/src/main.go
+++ Unix.ClientApp/src/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/docs", serveDocs)
+	http.HandleFunc("/docs/configuration", serveConfigDocs)
 	log.Info("Setting up application on port 8080.")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
@@ -17,9 +18,12 @@ func main() {
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/index.html")
-
 }
 
 func serveDocs(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/docs.html")
+}
+
+func serveConfigDocs(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/docpages/config.html")
 }
