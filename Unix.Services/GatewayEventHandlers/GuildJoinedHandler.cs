b7e93f90418597d40cc0ba875f02950a802c7b76
n-Ultima <nicbarclay05@gmail.com>
2021-12-09 22:54:11 -0600

    Forgot space

--- Unix.Services/GatewayEventHandlers/GuildJoinedHandler.cs
+++ Unix.Services/GatewayEventHandlers/GuildJoinedHandler.cs
@@ -43,7 +43,7 @@ public class GuildJoinedHandler : UnixService
                 catch (RestApiException)
                 {
                     await Bot.SendMessageAsync(e.Guild.OwnerId, new LocalMessage()
-                        .WithContent("Thank you for adding Unix! A few things to note:\n1. Join the Unix Discord Server for support and bug issues(http://www.ultima.one/unix)\n2. Use the `/configure-modrole` and `/configure-adminrole` to setup permissions. Until this is done only the owner can perform moderation commands.\n3. If this guild is found to be in violation of Discord's TOS or any other applicable laws, the guild will be blacklisted from using Unix.\n4. Please run commands to setup your guild fully! All of these commands start with the `/configure` prefix, such as `/configure-modrole`."));
+                        .WithContent("Thank you for adding Unix! A few things to note:\n1. Join the Unix Discord Server for support and bug issues(http://www.ultima.one/unix)\n2. Use the `/setup-guild` command to setup your guild(only the owner can do this).\n3. If this guild is found to be in violation of Discord's TOS or any other applicable laws, the guild will be blacklisted from using Unix.\n4. Please run commands to setup your guild fully! All of these commands start with the `/configure` prefix, such as `/configure-modrole`."));
                 }
 
                 unixContext.GuildConfigurations.Add(new GuildConfiguration
