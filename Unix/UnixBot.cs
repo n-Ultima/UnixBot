e94c731f71b3f26ca0d148a643d794a317dd51e6
n-Ultima <nicbarclay05@gmail.com>
2021-12-22 23:31:06 -0600

    Update deps

--- Unix/UnixBot.cs
+++ Unix/UnixBot.cs
@@ -1,25 +1,8 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Disqord;
-using Disqord.Bot;
-using Disqord.Bot.Sharding;
-using Disqord.Rest;
-using Disqord.Sharding;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Qmmands;
-using Serilog;
-using Unix.Common;
-using Unix.Services.Core;
-
+﻿
 namespace Unix;
 
 public class UnixBot
 {
-    
+
 
 }
