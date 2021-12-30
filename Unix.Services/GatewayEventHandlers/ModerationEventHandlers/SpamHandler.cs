b726be719e0a423f2c4bf52beaac8f4458a05604
n-Ultima <nicbarclay05@gmail.com>
2021-12-30 00:44:38 -0600

    Refactor spam handler to fix edgecase

--- Unix.Services/GatewayEventHandlers/ModerationEventHandlers/SpamHandler.cs
+++ Unix.Services/GatewayEventHandlers/ModerationEventHandlers/SpamHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,60 +18,15 @@ namespace Unix.Services.GatewayEventHandlers;
 
 public class SpamHandler : UnixService
 {
-    private readonly ConcurrentDictionary<CachedMember, int> SpamDictionary = new();
+    private readonly ConcurrentDictionary<CachedMember, List<IUserMessage>> SpamDictionary = new();
     public static ConcurrentDictionary<Snowflake, int> AmountOfMessages = new();
     private readonly IModerationService _moderationService;
     private readonly IGuildService _guildService;
-    private Timer SpamTimer;
+
     public SpamHandler(IServiceProvider serviceProvider, IGuildService guildService, IModerationService moderationService) : base(serviceProvider)
     {
         _guildService = guildService;
         _moderationService = moderationService;
-        SetTimer();
-    }
-
-    private void SetTimer()
-    {
-        var timeSpan = TimeSpan.FromSeconds(3);
-        SpamTimer = new Timer(_ => Task.Run(HandleTimerAsync), null, timeSpan, timeSpan);
-    }
-
-    private async Task HandleTimerAsync()
-    {
-        foreach (var entry in SpamDictionary)
-        {
-            var member = entry.Key; // The member
-            var guildConfig = await _guildService.FetchGuildConfigurationAsync(member.GuildId); // Get the guild config
-            var spamAmt = AmountOfMessages[guildConfig.Id];// The amount of messages considered spam. 
-            var warnUsers = SpamDictionary
-                .Where(x => x.Value >= spamAmt)
-                .Where(x => x.Key.GuildId == guildConfig.Id)
-                .ToList();
-            foreach (var user in warnUsers)
-            {
-                if (!SpamDictionary.TryGetValue(user.Key, out _))
-                {
-                    continue;
-                }
-                await _moderationService.CreateInfractionAsync(user.Key.GuildId, Bot.CurrentUser.Id, user.Key.Id, InfractionType.Warn, "Spamming messages", false, null);
-                if (!SpamDictionary.TryRemove(user.Key, out _))
-                {
-                    Log.Logger.Error("Failed to remove {key} from the spam dictionary.", user.Key);
-                }
-            }
-
-            var removeUsers = SpamDictionary
-                .Where(x => x.Value < spamAmt)
-                .Where(x => x.Key.GuildId == guildConfig.Id)
-                .ToList();
-            foreach (var user in removeUsers)
-            {
-                if (!SpamDictionary.TryRemove(user.Key, out _))
-                {
-                    Log.Logger.Error("Failed to remove {key} from the spam dictionary.", user.Key);
-                }
-            }
-        }
     }
 
     protected override async ValueTask OnMessageReceived(BotMessageReceivedEventArgs eventArgs)
@@ -85,6 +41,7 @@ public class SpamHandler : UnixService
         {
             return;
         }
+
         if (!AmountOfMessages.TryGetValue(eventArgs.GuildId.Value, out _))
         {
             var guildConfigTemp = await _guildService.FetchGuildConfigurationAsync(eventArgs.GuildId.Value);
@@ -95,21 +52,24 @@ public class SpamHandler : UnixService
 
             AmountOfMessages.TryAdd(eventArgs.GuildId.Value, guildConfigTemp.AmountOfMessagesConsideredSpam);
         }
+
         var guildConfig = await _guildService.FetchGuildConfigurationAsync(eventArgs.GuildId.Value);
         if (guildConfig == null)
         {
             return;
         }
-        var guild = Bot.GetGuild(guildConfig.Id);
+
         if (!eventArgs.Member.RoleIds.Any())
         {
             goto botCheck;
         }
-        if (eventArgs.Member.RoleIds.Contains(guildConfig.ModeratorRoleId) || eventArgs.Member.RoleIds.Contains(guildConfig.AdministratorRoleId))
+
+        if (eventArgs.Member.RoleIds.Contains(guildConfig.ModeratorRoleId) || eventArgs.Member.RoleIds.Contains(guildConfig.AdministratorRoleId) || eventArgs.Member.GetGuild().OwnerId == eventArgs.Member.Id)
         {
             return;
         }
-    botCheck:
+
+        botCheck:
         if (message.Author.IsBot)
         {
             return;
@@ -119,6 +79,41 @@ public class SpamHandler : UnixService
         {
             return;
         }
-        SpamDictionary.AddOrUpdate(eventArgs.Member, 1, (_, oldValue) => oldValue + 1);
+
+        var spamAmt = SpamDictionary.TryGetValue(eventArgs.Member, out var messages);
+        if (!spamAmt)
+        {
+            List<IUserMessage> userMessages = new();
+            userMessages.Add(message);
+            SpamDictionary.TryAdd(eventArgs.Member, userMessages);
+        }
+        else
+        {
+            List<IUserMessage> updatedMessages = new();
+            updatedMessages.AddRange(messages);
+            updatedMessages.Add(message);
+            SpamDictionary[eventArgs.Member] = updatedMessages;
+            if (SpamDictionary[eventArgs.Member].Count == 3)
+            {
+                await CheckIfSpamAsync(eventArgs.Member, SpamDictionary[eventArgs.Member]);
+            }
+        }
+    }
+
+    private async Task CheckIfSpamAsync(CachedMember member, List<IUserMessage> messages)
+    {
+        if (messages.Count != 3)
+        {
+            throw new Exception("Method must be called when the user has sent at least 3 messages.");
+        }
+
+        var message1 = messages[0];
+        var message2 = messages[1];
+        var message3 = messages[2];
+        if (message3.CreatedAt() - message2.CreatedAt() < TimeSpan.FromSeconds(1) && message2.CreatedAt() - message1.CreatedAt() < TimeSpan.FromSeconds(1))
+        {
+            await _moderationService.CreateInfractionAsync(member.GuildId, Bot.CurrentUser.Id, member.Id, InfractionType.Warn, "Spamming messages", false, null);
+            SpamDictionary.TryRemove(member, out _);
+        }
     }
 }
